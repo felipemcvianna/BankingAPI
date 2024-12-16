e471266b20c9c813d4946ef228b0c01e04826210
felipemcvianna <felipemiranda0211@gmail.com>
2024-12-16 20:46:22 -0300

    Adicionando UserIdentifier na tabela de clientes

--- Banking.Domain/Entities/Cliente.cs
+++ Banking.Domain/Entities/Cliente.cs
@@ -3,9 +3,10 @@ namespace Banking.Domain.Entities;
 public class Cliente
 {
     public int Id { get; set; }
-    public int ContaId { get; set; }
+    public int NumeroConta { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
     public string CPF { get; set; }
     public string Senha { get; set; }
+    public Guid UserIdentifier { get; set; }
 }
--- Banking.Domain/Entities/Conta.cs
+++ Banking.Domain/Entities/Conta.cs
@@ -3,7 +3,9 @@ namespace Banking.Domain.Entities;
 public class Conta
 {
     public int Id { get; set; }
+    public int NumeroAgencia {  get; set; }
+    public int NumeroBanco { get; set; }
     public int NumeroConta { get; set; }
     public double Saldo { get; set; }
     public DateTime DataCriacao { get; set; }
-}
+}   
