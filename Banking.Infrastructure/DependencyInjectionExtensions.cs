5384e745c13d26d60125b568a2572b833b6e06e8
felipemcvianna <felipemiranda0211@gmail.com>
2024-12-16 20:49:31 -0300

    Adicionando injeção de dependência para criação do token de acesso

--- Banking.Infrastructure/DependencyInjectionExtensions.cs
+++ Banking.Infrastructure/DependencyInjectionExtensions.cs
@@ -1,7 +1,11 @@
 using Banking.Domain.Repositories;
 using Banking.Domain.Repositories.Cliente;
+using Banking.Domain.Repositories.Conta;
+using Banking.Domain.Seguranca.Tokens.Generate;
 using Banking.Infrastructure.Data.Repositories;
 using Banking.Infrastructure.Data.Repositories.Cliente;
+using Banking.Infrastructure.Data.Repositories.Conta;
+using Banking.Infrastructure.Seguranca.Tokens.Acesso.Generator;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,6 +18,7 @@ public static class DependencyInjectionExtensions
     {
         services.AddDbContext(configuration);
         services.AddRepositories();
+        services.AddTokens(configuration);
         return services;
     }
 
@@ -26,10 +31,25 @@ public static class DependencyInjectionExtensions
 
     private static IServiceCollection AddRepositories(this IServiceCollection services)
     {
+        //CLIENTE REPOSITORIES
         services.AddScoped<IGravarClienteRepository, ClienteRepository>();
         services.AddScoped<ILerCLienteRepository, ClienteRepository>(); 
         services.AddScoped<IDeletarClienteRepository, ClienteRepository>(); 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        //CONTA REPOSITORIES
+        services.AddScoped<IGravarContaRepository, ContaRepository>();
+        services.AddScoped<ILerContaRepository, ContaRepository>();
+        return services;
+    }
+
+    private static IServiceCollection AddTokens(this IServiceCollection services, IConfiguration configuration)
+    {
+        var expiretionTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpirationTimeMinutes");
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+        services.AddScoped<IAcessTokenGenerator>(options => new AcessTokenGenerator(signingKey!, expiretionTimeMinutes));
+
         return services;
     }
 
