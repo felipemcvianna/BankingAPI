de44151ec2299eaaa849e2d205cf22e5159c54a9
felipemcvianna <felipemiranda0211@gmail.com>
2024-12-16 21:20:58 -0300

    caso de uso criar conta

---
+++ Banking.Application/UseCases/Conta/IRegistrarContaUseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banking.Communication.Response.Conta;
+
+namespace Banking.Application.UseCases.Conta
+{
+    public interface IRegistrarContaUseCase
+    {
+        public Task<ResponseRegistrarContaJson> Execute();
+    }
+}
---
+++ Banking.Application/UseCases/Conta/RegistrarContaUseCase.cs
@@ -0,0 +1,70 @@
+﻿using Banking.Communication.Requests.Cliente;
+using Banking.Communication.Response.Conta;
+using Banking.Domain.Repositories;
+using Banking.Domain.Repositories.Conta;
+using Banking.Exceptions.ExceptionBase;
+
+namespace Banking.Application.UseCases.Conta
+{
+    public class RegistrarContaUseCase : IRegistrarContaUseCase
+    {
+        private readonly IGravarContaRepository _gravarRepository;
+        private readonly ILerContaRepository _lerContaRepository;      
+
+        public RegistrarContaUseCase(IGravarContaRepository gravarRepository,
+            ILerContaRepository lerContaRepository)
+        {
+            _gravarRepository = gravarRepository;
+            _lerContaRepository = lerContaRepository;
+            
+        }
+
+        public async Task<ResponseRegistrarContaJson> Execute()
+        {          
+            var numeroBanco = ObterNumeroBanco();
+            var numeroAgencia = ObterNumeroAgencia();
+            var numeroConta = await GerarNumeroConta();
+            
+            var conta = new Domain.Entities.Conta
+            {
+                DataCriacao = DateTime.UtcNow,
+                NumeroAgencia = numeroAgencia,
+                NumeroBanco = numeroBanco,
+                NumeroConta = numeroConta,
+                Saldo = 0
+            };
+       
+            await ValidarSeContaExiste(conta.NumeroConta);
+            
+            await _gravarRepository.Add(conta);
+
+            return new ResponseRegistrarContaJson()
+            {
+                NumeroConta = conta.NumeroConta
+            };
+        }
+
+        private async Task ValidarSeContaExiste(int numeroConta)
+        {
+            var contaExistente = await _lerContaRepository.ExisteConta(numeroConta);
+            if (contaExistente)
+                throw new BusinessException("CONTA JÁ EXISTENTE");
+        }
+
+        private int ObterNumeroBanco()
+        {
+            return 260; 
+        }
+
+        private int ObterNumeroAgencia()
+        {
+            return 0001;
+        }
+
+        private async Task<int> GerarNumeroConta()
+        {          
+            var ultimoNumero = await _lerContaRepository.ObterUltimoNumeroConta();
+            return ultimoNumero + 1;
+        }
+    }
+}
