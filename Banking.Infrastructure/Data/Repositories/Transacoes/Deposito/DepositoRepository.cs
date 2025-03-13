41d43b892094fb12b121f9f3971b0f06728f4e8f
felipemcvianna <felipemiranda0211@gmail.com>
2025-03-12 21:33:12 -0300

    Repositorio para depositos

--- Banking.Infrastructure/Data/Repositories/Transacoes/Deposito/DepositoRepository.cs
+++ Banking.Infrastructure/Data/Repositories/Transacoes/Deposito/DepositoRepository.cs
@@ -1,8 +1,11 @@
-﻿using Banking.Domain.Repositories.Transacoes.Deposito;
+﻿using System.Globalization;
+using Banking.Domain.Repositories.Transacoes.Deposito;
+using Banking.Exceptions.ExceptionBase;
+using Microsoft.EntityFrameworkCore;
 
 namespace Banking.Infrastructure.Data.Repositories.Transacoes.Deposito
 {
-    public class DepositoRepository : IGravarDepositoRepository
+    public class DepositoRepository : IGravarDepositoRepository, ILerDepositosRepository
     {
         private readonly BankingDbContext _context;
 
@@ -12,5 +15,17 @@ namespace Banking.Infrastructure.Data.Repositories.Transacoes.Deposito
         }
 
         public async Task Add(Domain.Entities.Deposito deposito) => await _context.Depositos.AddAsync(deposito);
+
+        public async Task<List<Domain.Entities.Deposito>> GetAllDepositos(string cpfCliente) =>
+            await _context.Depositos.Where(x => x.CpfCliente == cpfCliente).ToListAsync();
+
+        public async Task<List<Domain.Entities.Deposito>> GetDepositosByPeriodo(DateTime startDate, DateTime endDate)
+        {
+            var variavelTeste = endDate.AddDays(1);
+
+            return await _context.Depositos
+                .Where(d => d.DataDeposito >= startDate && d.DataDeposito <= variavelTeste)
+                .ToListAsync();
+        }
     }
 }
